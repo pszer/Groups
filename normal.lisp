5fbed9dcb121e9d7885ec1f2739271eb909f275b
pszerszynski <alienmode1337@gmail.com>
2020-03-02 01:35:28 +0000

    normal subgroups

--- normal.lisp
+++ normal.lisp
@@ -4,3 +4,5 @@
   (and (subgroup-p sub super)
        (loop for inner-automorphism in (inner-automorphisms super) always
 	    (set-equal (elements sub) (elements (apply-map inner-automorphism sub))))))
+(defun normal-subgroups (group)
+  (remove-if-not #'(lambda (x) (normal-subgroup-p x group)) (subgroups group)))
